d6fcb5e02ab9f3956a65879ba52ce386671f36c1
LobnieYT <71641957+LobnieYT@users.noreply.github.com>
2026-09-17 22:21:25 +0300

    Delete release directory

--- release/snap/launcher.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# Лаунчер для snap (classic): suid-песочница Chromium не работает внутри
-# snap-монта, поэтому --no-sandbox (внешний confinement снапа сохраняется).
-exec "$SNAP/opt/cozymusic/yandexmusic" --no-sandbox "$@"
